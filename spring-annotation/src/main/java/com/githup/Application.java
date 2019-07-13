c057646b7214a546c293aec8e960c61b3204c0da
leo <13680385849@163.com>
2019-07-13 20:49:47 +0800

    提交类图

--- spring-annotation/src/main/java/com/githup/Application.java
+++ spring-annotation/src/main/java/com/githup/Application.java
@@ -1,8 +1,6 @@
 package com.githup;
 
 
-
-import com.githup.bean.Person;
 import com.githup.processor.MyBeanDefinitionRegistryPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,7 +9,5 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MyBeanDefinitionRegistryPostProcessor.class);
-        Person person = applicationContext.getBean(Person.class);
-        System.out.println(person);
     }
 }
